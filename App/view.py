085f712db3c2c9b7498303ffce1f64ed97c06cd6
SantiagoRC <s.rodriguez52@uniandes.edu.co>
2021-02-09 22:34:16 -0500

    cambios del Est 2

--- App/view.py
+++ App/view.py
@@ -81,14 +81,19 @@ while True:
 
 
         # TO-DO: Modificación de Est-2 en el Lab 2
+        print('Último libro cargado: ' + str(lt.lastElement(books)))
 
     elif int(inputs[0]) == 2:
         print("Cargando información de tags....")
         tags = loadTags()
         print('Total de tags cargados: ' + str(lt.size(tags)))
-    elif True:
+    elif int(inputs[0]) == 3:
+       print("Cargando información de Book-Tags...")
+       booktags = loadBookTags()
+       print('Total de Book-Tags cargados: ' + str(lt.size(booktags)))
         # TO-DO: Modificación de Est-2 en el Lab 2
-        pass
+
+        
 
     else:
         sys.exit(0)
